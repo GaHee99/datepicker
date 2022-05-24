ca1d4b680958ca7b46ccd8ca014ba47dedff35c5
Gahee99 <u1879@sookmyung.ac.kr>
2022-05-24 11:43:10 +0900

    new branch and new commit, i'will check this commit in github

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React, { useState } from 'react'
+import { StyleSheet, View, Text, Button, Platform } from 'react-native'
 
-export default function App() {
+import DateTimePicker from '@react-native-community/datetimepicker'
+import DatePicker1 from './component/DatePicker1'
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <DatePicker1 />
     </View>
-  );
+  )
 }
 
+// Kindacode.com
+// just add some styles to make our app look more beautiful
+// This is not the focus of this article
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
+    flex: 1,
     justifyContent: 'center',
+    padding: 50,
   },
-});
+  pickedDateContainer: {
+    padding: 20,
+    backgroundColor: '#eee',
+    borderRadius: 10,
+  },
+  pickedDate: {
+    fontSize: 18,
+    color: 'black',
+  },
+  btnContainer: {
+    padding: 30,
+  },
+  // This only works on iOS
+  datePicker: {
+    width: 320,
+    height: 260,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+})
+
+export default App
